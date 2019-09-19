07229764c3a4ecc4ec4f6f1dd3bc4270cef42db6
Paul Jensen <PJensen@pauls-mbp.home>
2019-09-19 18:15:30 -0400

    working through lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,41 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attr){
+    this.name = attr.name,
+    this.age = attr.age,
+    this.location = attr.location
+}
+speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+}
+}
+
+class Instructor extends Person {
+    constructor(instructorattr){
+        super(instructorattr);
+        this.specialty = instructorattr.specialty,
+        this.favLanguage = instructorattr.favLanguage,
+        this.catchPhrase = instructorattr.catchPhrase
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`;
+    }
+    grade(Student, subject){
+        return `${student.name} receives a perfect score on ${subject}.`;
+    }
+}
+
+class Student extends Person {
+    constructor(studentattr){
+        super(studentattr);
+        this.previousBackground = studentattr.previousBackground,
+        this.className = studentattr.className,
+        this.favSubjects = studentattr.favSubjects,
+    }
+    listSubjects(){
+        return `${this.favSubjects}`;
+    }
+    PRAssignment(Student, subject){
+        return `${Student.name} has submitted a PR for ${subject}`;
+    }
+}
